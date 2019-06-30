94f541f82449dfe60af4610ef67997fba6cffcd8
DATA_DEER <konto@datadeer.de>
2019-06-30 16:57:25 +0200

    * removed console.logs

--- src/components/header/menu.vue
+++ src/components/header/menu.vue
@@ -1,66 +1,78 @@
 <template>
-    <nav class="menu bg-black pb-8 pt-8 pl-3 pr-3 w-full">
-        <ul class="list-reset flex items-center flex-wrap">
-            <li class="ml-6">
-                <a class="block menu-link text-white text-4xl font-subtitle uppercase no-underline" href="/">Home</a>
-            </li>
-            <li v-for="item in menuItems" class="ml-24" v-bind:key="item.targetName">
-                <router-link class="block menu-link text-white text-base font-subtitle hover:text-blue-darker uppercase no-underline" :data-text="item.label" :to="{ name: item.targetName }" :class="{'text-white-barely-visible pointer-events-none': isDisabledRoute(item.targetName, menuItems)}" v-html="item.html"></router-link>
-            </li>
-        </ul>
-
-    </nav>
+  <nav class="menu bg-black pb-8 pt-8 pl-3 pr-3 w-full">
+    <ul class="list-reset flex items-center flex-wrap">
+      <li class="ml-6">
+        <a
+          class="block menu-link text-white text-4xl font-subtitle uppercase no-underline"
+          href="/"
+          >Home</a
+        >
+      </li>
+      <li v-for="item in menuItems" class="ml-24" v-bind:key="item.targetName">
+        <router-link
+          class="block menu-link text-white text-base font-subtitle hover:text-blue-darker uppercase no-underline"
+          :data-text="item.label"
+          :to="{ name: item.targetName }"
+          :class="{
+            'text-white-barely-visible pointer-events-none': isDisabledRoute(
+              item.targetName,
+              menuItems
+            )
+          }"
+          v-html="item.html"
+        ></router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-    export default {
-        name: "menu",
-        data() {
-            return {
-                menuItems:[
-                    {
-                        label:"Your Taste",
-                        html:"Your <br/> Taste",
-                        targetName:"getUserData"
-
-                    },
-                    {
-                        label:"Find Games",
-                        html:"Find <br/> Games",
-                        targetName: "search"
-                    }
-                ]
-            }
+export default {
+  name: "menu",
+  data() {
+    return {
+      menuItems: [
+        {
+          label: "Your Taste",
+          html: "Your <br/> Taste",
+          targetName: "getUserData"
         },
-        methods: {
-            isDisabledRoute(targetName, menuItems){
-                const elementHasTargetName = (element, targetName) => {
-                    return element.targetName === targetName;
-                }
-                const currentRouteName = this.$router.currentRoute.name;
-                const indexOfTargetNameInMenu = menuItems.findIndex((element) => elementHasTargetName(element, targetName));
-                const indexOfCurrentRouteInMenu = menuItems.findIndex((element) => elementHasTargetName(element, currentRouteName));
-                console.log(indexOfTargetNameInMenu);
-                console.log(menuItems.length -1);
-                return indexOfTargetNameInMenu  > indexOfCurrentRouteInMenu;
-            }
+        {
+          label: "Find Games",
+          html: "Find <br/> Games",
+          targetName: "search"
         }
+      ]
+    };
+  },
+  methods: {
+    isDisabledRoute(targetName, menuItems) {
+      const elementHasTargetName = (element, targetName) => {
+        return element.targetName === targetName;
+      };
+      const currentRouteName = this.$router.currentRoute.name;
+      const indexOfTargetNameInMenu = menuItems.findIndex(element =>
+        elementHasTargetName(element, targetName)
+      );
+      const indexOfCurrentRouteInMenu = menuItems.findIndex(element =>
+        elementHasTargetName(element, currentRouteName)
+      );
+      return indexOfTargetNameInMenu > indexOfCurrentRouteInMenu;
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./../../assets/scss/animations.scss";
+@import "./../../assets/scss/mixins.scss";
 
-    @import "./../../assets/scss/animations.scss";
-    @import "./../../assets/scss/mixins.scss";
+.menu-link:hover {
+  @include chromatic3dEffectText();
+  color: transparent;
+}
 
-    .menu-link:hover {
-        @include chromatic3dEffectText();
-        color:transparent;
-    }
-
-    .router-link-active {
-        @include chromatic3dEffectText();
-    }
-
-
+.router-link-active {
+  @include chromatic3dEffectText();
+}
 </style>
--- src/components/memeface/message.vue
+++ src/components/memeface/message.vue
@@ -1,61 +1,51 @@
 <template>
-    <section class="message pl-6 pr-6">
+  <section class="message pl-6 pr-6">
+    <div class="flex flex-row justify-start mb-3">
+      <lenny class="w-12 h-12"></lenny>
+    </div>
 
-        <div class="flex flex-row justify-start mb-3">
-            <lenny class="w-12 h-12"></lenny>
-        </div>
-
-        <article class="relative bg-black border-white border-2 ml-2 animated fadeIn">
-            <div class="message-triangle-position absolute border-2 border-white h-12 w-8 z-0"></div>
-            <div class="relative h-auto z-20 pt-6 pb-6 pl-6 pr-6 bg-black text-white font-sans">
-                <slot></slot>
-            </div>
-        </article>
-
-    </section>
+    <article
+      class="relative bg-black border-white border-2 ml-2 animated fadeIn"
+    >
+      <div
+        class="message-triangle-position absolute border-2 border-white h-12 w-8 z-0"
+      ></div>
+      <div
+        class="relative h-auto z-20 pt-6 pb-6 pl-6 pr-6 bg-black text-white font-sans"
+      >
+        <slot></slot>
+      </div>
+    </article>
+  </section>
 </template>
 
 <script>
+import lenny from "@/components/memeface/lenny";
 
-    import lenny from "@/components/memeface/lenny";
-
-    export default {
-        name: "message",
-        props: [],
-        components: {
-            lenny,
-        },
-        data() {
-            return {
-            }
-        },
-        methods: {
-
-            clear(){
-                console.log("did it clear");
-            }
-
-        },
-        created(){
-        }
-    }
+export default {
+  name: "message",
+  props: [],
+  components: {
+    lenny
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {}
+};
 </script>
 
 <style lang="scss" scoped>
+.message-triangle-position {
+  left: 1rem;
+  top: -1rem;
+  transform: rotate(-65deg);
+}
 
-    .message-triangle-position {
-
-        left:1rem;
-        top:-1rem;
-        transform: rotate(-65deg);
-    }
-
-    .close-button-position {
-
-        right:-0.5rem;
-        top:-0.7rem;
-        line-height: 1.2;
-
-    }
-
+.close-button-position {
+  right: -0.5rem;
+  top: -0.7rem;
+  line-height: 1.2;
+}
 </style>
